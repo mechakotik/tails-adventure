0ceb88d86c78cb5d45776783bd4d7313a9003fc1
mechakotik <entity499@yandex.by>
2023-06-07 20:19:48 +0300

    Level select on world map

--- include/map_screen.h
+++ include/map_screen.h
@@ -14,8 +14,14 @@ private:
     void setActivePoints();
     void setPointNeighbours();
 
+    void handleInput();
+    void draw();
+
     TA_Tilemap tilemap;
+    TA_CommonController controller;
     std::vector<TA_MapPoint*> points;
+    TA_Sprite tailsIcon;
+    TA_MapPoint* currentPoint;
 
 public:
     void init() override;
@@ -25,20 +31,26 @@ public:
 
 class TA_MapPoint {
 private:
+    const double lightTime = 15, appearTime = 3;
+
     TA_Sprite sprite;
     TA_Point position;
     std::string name = "", path = "";
     std::array<TA_MapPoint*, TA_DIRECTION_MAX> neighbours;
 
+    bool active = false;
+    double timer = 0;
+
 public:
     TA_MapPoint(std::string name, std::string path, TA_Point position);
     void setNeighbour(TA_Direction direction, TA_MapPoint* neighbour) {neighbours[direction] = neighbour;}
-    void activate() {sprite.setAnimation("active");}
-    void draw() {sprite.draw();}
+    void activate() {active = true;}
+    void draw();
 
     TA_Point getPosition() {return position;}
     std::string getName() {return name;}
-    bool isActive() {return sprite.isAnimated();}
+    std::string getPath() {return path;}
+    bool isActive() {return active;}
     TA_MapPoint* getNeighbour(TA_Direction direction) {return neighbours[direction];}
 };
 
--- src/map_screen.cpp
+++ src/map_screen.cpp
@@ -4,6 +4,12 @@
 void TA_MapScreen::init()
 {
     tilemap.load("worldmap/map.tmx");
+    controller.load();
+
+    tailsIcon.load("worldmap/tails_icon.png", 7, 7);
+    tailsIcon.loadAnimationsFromFile("worldmap/tails_icon.xml");
+    tailsIcon.setAnimation("blink");
+
     loadPoints();
 }
 
@@ -16,14 +22,15 @@ void TA_MapScreen::loadPoints()
 
 void TA_MapScreen::appendPoints()
 {
-    points.push_back(new TA_MapPoint("tails'\nhouse", "", TA_Point(0, 0)));
-    points.push_back(new TA_MapPoint("poloy\nforest", "maps/pf/pf1", TA_Point(0, 0)));
-    points.push_back(new TA_MapPoint("volcanic\ntunnel", "maps/vt/vt1", TA_Point(0, 0)));
+    points.push_back(new TA_MapPoint("tails'\nhouse", "", TA_Point(122, 97)));
+    points.push_back(new TA_MapPoint("poloy\nforest", "maps/pf/pf1", TA_Point(106, 89)));
+    points.push_back(new TA_MapPoint("volcanic\ntunnel", "maps/vt/vt1", TA_Point(146, 73)));
+    currentPoint = points[0];
 }
 
 void TA_MapScreen::setActivePoints()
 {
-    int mask = TA::save::getSaveParameter("level_mask");
+    int mask = TA::save::getSaveParameter("area_mask");
     for(int level = 0; level < (int)points.size(); level ++) {
         if(mask & (1 << level)) {
             points[level]->activate();
@@ -45,8 +52,35 @@ void TA_MapScreen::setPointNeighbours()
 
 TA_ScreenState TA_MapScreen::update()
 {
-    tilemap.draw(0);
+    handleInput();
+    draw();
+
+    if(controller.isJustPressed(TA_BUTTON_A) || controller.isJustPressed(TA_BUTTON_B)) {
+        TA::levelPath = currentPoint->getPath();
+        return TA_SCREENSTATE_GAME;
+    }
     return TA_SCREENSTATE_CURRENT;
+}
+
+void TA_MapScreen::handleInput()
+{
+    controller.update();
+    if(controller.isJustChangedDirection()) {
+        TA_Direction direction = controller.getDirection();
+        if(direction != TA_DIRECTION_MAX && currentPoint->getNeighbour(direction) != nullptr) {
+            currentPoint = currentPoint->getNeighbour(direction);
+        }
+    }
+    tailsIcon.setPosition(currentPoint->getPosition() + TA_Point(-2, 8));
+}
+
+void TA_MapScreen::draw()
+{
+    tilemap.draw(0);
+    for(int pos = 1; pos < (int)points.size(); pos ++) {
+        points[pos]->draw();
+    }
+    tailsIcon.draw();
 }
 
 void TA_MapScreen::quit()
@@ -65,6 +99,21 @@ TA_MapPoint::TA_MapPoint(std::string name, std::string path, TA_Point position)
         neighbours[direction] = nullptr;
     }
 
-    sprite.load("worldmap/point.png");
+    sprite.load("worldmap/selection.png");
     sprite.setPosition(position);
+}
+
+void TA_MapPoint::draw()
+{
+    if(!active) {
+        return;
+    }
+    timer = std::fmod(timer + TA::elapsedTime, lightTime * 2);
+    if(timer < lightTime) {
+        sprite.setAlpha(255 * timer / appearTime);
+    }
+    else {
+        sprite.setAlpha(255 - 255 * (timer - lightTime) / appearTime);
+    }
+    sprite.draw();
 }
