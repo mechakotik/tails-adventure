8b8d9fdc67de7aa112cb28039b45d19c2ac1f272
mechakotik <mechakotik@gmail.com>
2024-02-17 17:59:46 +0300

    CI2 tilemap

--- src/devmenu_screen.cpp
+++ src/devmenu_screen.cpp
@@ -14,6 +14,7 @@ void TA_DevmenuScreen::init()
     levels.emplace_back("maps/pm/pm3");
     levels.emplace_back("maps/pm/pm4");
     levels.emplace_back("maps/ci/ci1");
+    levels.emplace_back("maps/ci/ci2");
     normalFont.load("fonts/devmenu.png", 7, 9);
     normalFont.setMapping(mapping);
     selectedFont.load("fonts/devmenu_selected.png", 7, 9);
