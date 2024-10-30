8a2a4c575730a89fd15e00544746798f2cf99dd9
Andrei Sabalenka <mechakotik@gmail.com>
2024-10-30 22:26:11 +0300

    CMake NDK wip

--- android/app/src/main/java/com/mechakotik/tailsadventure/TailsAdventure.java
+++ android/app/src/main/java/com/mechakotik/tailsadventure/TailsAdventure.java
@@ -1,3 +1,13 @@
 package com.mechakotik.tailsadventure;
 import org.libsdl.app.SDLActivity;
-public class TailsAdventure extends SDLActivity {}
+
+public class TailsAdventure extends SDLActivity {
+    protected String[] getLibraries() {
+        return new String[] {
+                "SDL3",
+                "SDL3_image",
+                "SDL3_mixer",
+                "tails-adventure"
+        };
+    }
+}
