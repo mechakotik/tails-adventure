0e4a7d137757bd88c5cc5d54c73fc8647a825e7f
Andrei Sabalenka <mechakotik@gmail.com>
2025-07-22 23:29:08 +0300

    CC3 tilemap

--- src/ui/devmenu_screen.cpp
+++ src/ui/devmenu_screen.cpp
@@ -35,6 +35,7 @@ void TA_DevmenuScreen::init() {
     levels.emplace_back("maps/pv/pv2");
     levels.emplace_back("maps/cc/cc1");
     levels.emplace_back("maps/cc/cc2");
+    levels.emplace_back("maps/cc/cc3");
     normalFont.loadFont("fonts/devmenu.toml");
     selectedFont.loadFont("fonts/devmenu_selected.toml");
 }
