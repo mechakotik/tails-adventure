26f3e286787cc64a3a831f94119326c96c91d2d9
Andrei Sabalenka <mechakotik@gmail.com>
2026-01-08 17:31:05 +0300

    BF3 tilemap

--- src/ui/devmenu_screen.cpp
+++ src/ui/devmenu_screen.cpp
@@ -38,6 +38,7 @@ void TA_DevmenuScreen::init() {
     levels.emplace_back("maps/cc/cc3");
     levels.emplace_back("maps/bf/bf1");
     levels.emplace_back("maps/bf/bf2");
+    levels.emplace_back("maps/bf/bf3");
     normalFont.loadFont("fonts/devmenu.toml");
     selectedFont.loadFont("fonts/devmenu_selected.toml");
 }
